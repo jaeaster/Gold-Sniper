d9fd83362076538380e8e44f946728fc9ab60dfb
Gabriel Von Meier <vonmeier@umail.ucsb.edu>
2015-05-31 13:16:32 -0700

    added code in views.py on app start to set host to '0.0.0.0' and PORT to 5000. Attempt to fix R10 timeout error from heroku

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, url_for
 from GoldSniper import *
 import getpass
@@ -51,7 +52,9 @@ def submit():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)    
+	# Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=True)   
 
 
 
