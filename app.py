83922612a8b3c966af5ba081fbd8e6cda2e7ea48
Gabriel Von Meier <vonmeier@umail.ucsb.edu>
2015-05-31 17:00:55 -0700

    first attempt at setting up database, added code for simple SQL db with table of users, changed name of app from views.py to app.py

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 from flask import Flask, render_template, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 from GoldSniper import *
 import getpass
 from pytz import utc #UTC is timezone for scheduler 
 from apscheduler.schedulers.background import BackgroundScheduler
 #from apscheduler.jobstores.mongodb import MongoDBJobStore
-#from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
 
 
 jobstores = {
 	#'mongo': MongoDBJobStore()
+	'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
 }
 
 executors = {
@@ -34,6 +38,28 @@ sniper = GoldSniper()
 #create web app instance
 app = Flask(__name__)
 
+#DATABASE  vvv
+#got instructions for creating db from: https://github.com/miguelgrinberg/Flask-Migrate
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# create SQLAlchemy database instance
+db = SQLAlchemy(app)
+#create Migrate instance
+migrate = Migrate(app, db)
+#create Manager instance
+#can now do migrations in terminal with -> python app.py db migrate
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+#create database model
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(128))
+
+
+
 #routes index.html to the root of the web server
 @app.route('/')
 def index():
@@ -46,7 +72,7 @@ def submit():
 	password = request.form['password']
 	quarter = request.form['quarter']
 	enroll_code = request.form['enroll_code']
-	pass_time = request.form['passtime'] #MM/DD/YY HH:MM, 2009-05-30 HH:MM:SS
+	pass_time = request.form['passtime'] #MM/DD/YY HH:MM -> 2009-05-30 HH:MM:SS
 	pass_time_arg = '20' + pass_time[6:8] + '-' + pass_time[0:2] + '-' + pass_time[3:5] + ' ' + pass_time[9:] + ':00'
 	scheduler.add_job(sniper.goldSniper, 'date', run_date=pass_time_arg, args=[username,password,quarter,enroll_code])
 	return render_template('index.html')
@@ -54,25 +80,9 @@ def submit():
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)   
+	app.run(host='0.0.0.0', port=port, debug=True)
+	manager.run()   
 
 
 
 
-#other shit that was in app.py:
-"""username = str(raw_input("Please enter GOLD username: "))
-password = getpass.getpass("Please enter your password: ")
-
-username = "vonmeier"
-password = "C871d97d"
-quarter = "20154"
-enroll_code = "58677"
-sniper = GoldSniper()
-sniper.login(username,password)
-#sniper.listLinks()
-sniper.setQuarter("20154") #Fall
-sniper.snipeCourse("58677")
-
-
-#enroll_code = 58677
-"""
