ebc30217a3eb28cd27260c315bdca3f4d4f8847f
Jonathan Easterman <jonathaneasterman@gmail.com>
2015-10-18 22:20:14 -0700

    Push all files to heroku

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import os
 from flask import Flask, render_template, request, url_for
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
 from GoldSniper import *
 import getpass
 from pytz import timezone
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
-#create web app instance
+import logging
+
+# Create web app instance
 app = Flask(__name__)
 
-#Looks to config.py for config options
+# Looks to config.py for config options
 app.config.from_object('config')
 
-# The following dictionaries configure the BackgroundScheduler instance
+# Configuration for logs
+logging.basicConfig()
+
+# The following dictionaries configure
+# the BackgroundScheduler instance
+
 jobstores = {
 	'default': SQLAlchemyJobStore(url='sqlite:///app.db')
 }
@@ -29,59 +33,47 @@ job_defaults = {
 	'max_instances': 3
 }
 
-#fix for error: No handlers could be found for logger "apscheduler.executors.default"
-import logging
-logging.basicConfig()
 
-#instantiate scheduler with config options as the arguments
-scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone = timezone('US/Pacific'))
+# Instantiate scheduler with config options as the arguments
+scheduler = BackgroundScheduler(jobstores=jobstores,
+								executors=executors,
+								job_defaults=job_defaults,
+								timezone = timezone('US/Pacific'))
 scheduler.start()
 
-#DATABASE 
-#got instructions for creating db from: https://github.com/miguelgrinberg/Flask-Migrate
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
-# create SQLAlchemy database instance
-db = SQLAlchemy(app)
-
-#create Migrate instance
-migrate = Migrate(app, db)
-
-#create Manager instance
-#can now do migrations in terminal with -> python app.py db migrate
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
-
-#create database model
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(128))
-
-
-
-#routes index.html to the root of the web server
+# Routes index.html to the root of the web server
 @app.route('/')
 def index():
 	return render_template('index.html', title='Home')
 
-#Calls submit function when someone makes a POST request
+# Calls submit function when someone makes a POST request
 @app.route('/submit/', methods=['POST'])
 def submit():
 	username = request.form['username']
 	password = request.form['password']
 	quarter = request.form['quarter']
 	enroll_code = request.form['enroll_code']
-	pass_time = request.form['passtime'] #MM/DD/YY HH:MM -> 2009-05-30 HH:MM:SS
-	pass_time_arg = '20' + pass_time[6:8] + '-' + pass_time[0:2] + '-' + pass_time[3:5] + ' ' + pass_time[9:] + ':00'
-	scheduler.add_job(goldSniper, 'date', run_date=pass_time_arg, args=[username,password,quarter,enroll_code])
+	pass_time = request.form['passtime']
+
+	# Converts input format to usable format
+	# e.g. MM/DD/YY HH:MM -> YYYY-MM-DD HH:MM:SS
+	pass_time_arg = ('20' + pass_time[6:8] + '-' + pass_time[0:2]
+					+ '-' + pass_time[3:5] + ' ' + pass_time[9:] 
+					+ ':00')
+	# save job in scheduler database
+	scheduler.add_job(goldSniper,
+					  'date',
+					  run_date=pass_time_arg,
+					  args=[username,password,quarter,enroll_code]
+					  )
+
 	return render_template('index.html', title="Success")
 
 # Basically, if we are using app.py as the main module
 # i.e. If we execute 'python app.py'
 if __name__ == '__main__':
-	# Bind to PORT if defined, otherwise default to 5000
 
+	# Bind to PORT if defined, otherwise default to 5000
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port, debug=True)
-	manager.run()
